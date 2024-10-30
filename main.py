08e285e03b01cc982c1804397dc38c6a2615a2de
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-10-29 20:53:47 -0400

    instantiate null driver

--- main.py
+++ main.py
@@ -64,7 +64,8 @@ class UdioMusicBot:
 
         logger.info(f"Initializing UdioMusicBot with email: {self.email[:3]}...{self.email[-10:]}")
         self.setup_driver()
-
+        self.driver = None
+        
     def setup_driver(self):
         download_dir = os.getcwd() + "/song_downloads/"  # Adjust to your desired path
         try:
